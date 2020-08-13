d087be55f332f0507017ddebdcd8508c74654235
TJ Sornet <tsornet@gmail.com>
2020-08-12 19:50:13 -0700

    Added input fields

---
+++ src/components/Form.vue
@@ -0,0 +1,33 @@
+<template>
+    <form>
+        <h1>{{ title }}</h1>
+        <InputField label="Email" name="email" id="email" type="email" />
+        <InputField label="First Name" name="firstName" type="text" />
+        <InputField label="Last Name" name="lastName" type="text" />
+        <InputField label="Country" name="country" type="text" />
+        <InputField label="Company" name="company" type="text" />
+        <InputField label="Password" name="password" type="password" />
+        <InputField label="Confirm Password" name="confirmPassword" type="password" />
+        <button v-bind:disabled="isButtonDisabled">Button</button>
+    </form>
+</template>
+
+<script>
+import InputField from "./InputField";
+
+export default {
+    props: {
+        title: String,
+        isButtonDisabled: Boolean
+    },
+    components: {
+        InputField
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        text-align: center;
+    }
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="input-form">
+        <label class="input-label">{{ label }}</label>
+        <input class="input-field" :type="type" :name="name" />
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        props: {
+            label: String,
+            type: String,
+            name: String
+        }
+    }
+</script>
+
+<style scoped>
+    .input-form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding: 20px;
+        margin: auto;
+        width: 25rem;
+    }
+    .input-label {
+        text-align: start;
+        width: 150px;
+        margin-right: 5px;
+    }
+    .input-field {
+        appearance: none;
+        -moz-appearance: none;
+        -webkit-appearance: none;
+        border-radius: 0;
+        box-shadow: none;
+        padding: 10px;
+    }
+</style>
